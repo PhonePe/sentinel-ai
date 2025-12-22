0b986c2bdd18eaeb03f013ad546bfc403598c558
Santanu Sinha <santanu.sinha@gmail.com>
2025-12-22 16:22:14 +0530

    Added capability to force structured output in configured agents

--- sentinel-ai-configured-agents/src/main/java/com/phonepe/sentinelai/configuredagents/ModelFactory.java
+++ sentinel-ai-configured-agents/src/main/java/com/phonepe/sentinelai/configuredagents/ModelFactory.java
@@ -3,7 +3,9 @@ package com.phonepe.sentinelai.configuredagents;
 import com.phonepe.sentinelai.core.model.Model;
 
 /**
- *
+ * A generic interface to dynamically create models on the fly.
+ * The model from the parent agent setup will be passed as the defaultModel, implementation can decide to return it or
+ * throw an exception based on the circumstances.
  */
 @FunctionalInterface
 public interface ModelFactory {
