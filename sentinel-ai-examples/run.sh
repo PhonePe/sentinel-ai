3b827fd0fbb422cb860a8a0818451e78dd0076d6
Guruprasad Sridharan <guruprasad.sridharan@gmail.com>
2026-04-04 21:14:04 +0530

    Add DEBUG env var

--- sentinel-ai-examples/run.sh
+++ sentinel-ai-examples/run.sh
@@ -6,4 +6,8 @@ if [ "$BUILD" == "true" ]; then
   mvn clean install -DskipTests
 fi
 
-java "-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=*:5005" -jar target/sentinel-ai-examples-*-cli.jar
+if [ "$DEBUG" == "true" ]; then
+  export JAVA_TOOL_OPTIONS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=*:5005"
+fi
+
+java $JAVA_TOOL_OPTIONS -jar target/sentinel-ai-examples-*-cli.jar
